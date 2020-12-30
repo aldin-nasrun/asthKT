b5a17e2bb45d2425ca20e845aa2eab8ac2a5af1d
Violin <rizki_violin_radhiyan@git.com>
2020-12-30 15:22:56 +0700

    logout button back to login not programatically

--- app/src/main/java/com/quick/asthkt/views/SettingFragment.kt
+++ app/src/main/java/com/quick/asthkt/views/SettingFragment.kt
@@ -1,60 +1,50 @@
 package com.quick.asthkt.views
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.quick.asthkt.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var btn_logout : Button
+    val auth = FirebaseAuth.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        var v : View = inflater.inflate(R.layout.fragment_setting, container, false)
+        btn_logout = v.findViewById(R.id.btn_logout)
+        return v
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btn_logout.setOnClickListener {
+            logoutUser()
+        }
     }
+    private fun logoutUser(){
+        auth.signOut().also {
+        System.exit(0)
+        }
+    }
+//    fun triggerRebirth(context: Context, nextIntent: Intent?) {
+//        val intent = Intent(context, LoginActivity::class.java)
+//        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+//        intent.putExtra(KEY_RESTART_INTENT, nextIntent)
+//        context.startActivity(intent)
+//        if (context is Activity) {
+//            (context as Activity).finish()
+//        }
+//        Runtime.getRuntime().exit(0)
+//    }
 }
